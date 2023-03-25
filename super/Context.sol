e6dfebd5c70d8c31ec69ee6bae39463e39139c8e
Protradex <125808151+Protradex@users.noreply.github.com>
2023-03-25 12:36:56 +0800

    Add files via upload

---
+++ super/Context.sol
@@ -0,0 +1,15 @@
+/**
+ *Submitted for verification at BscScan.com on 2022-07-11
+*/
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
